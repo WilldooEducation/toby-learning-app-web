34ae5d88be24f1b2e2aeff7e043b32ca3fbef294
surendar <surendar@willdoo.com>
2024-05-04 21:13:59 +0530

    [NEW] story screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
+import Head from 'next/head'
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@material-tailwind/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,22 @@
 import Image from "next/image";
 import { Button } from "@material-tailwind/react";
 import styles from "./index.module.scss";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className={styles.main}>
-      <div className={styles.billboard}>
+      <div className={styles.splash__container}>
         <Image
           fill
-          src="/images/billboard.svg"
-          style={{ objectFit: "cover" }}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          src="/images/toby.svg"
+          style={{ objectFit: "contain" }}
           alt="billboard"
         />
       </div>
-
-      <div className={styles.conversation__block}>
-        <div className={styles["conversation__message"]}>
-          <div className={styles.avatar}>
-            <Image
-              fill
-              src="/images/girl.svg"
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              alt="billboard"
-            />
-          </div>
-          <div className={styles.message}>
-            <p>
-              {`"Hey Rohan, did you see the playground next to school? It's all dug up now!"`}
-            </p>
-          </div>
-        </div>
-        <div className={styles["conversation__message--reverse"]}>
-          <div className={styles.avatar}>
-            <Image
-              fill
-              src="/images/boy.svg"
-              style={{ objectFit: "cover" }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              alt="billboard"
-            />
-          </div>
-          <div className={styles.message}>
-            <p>{`Yeah, I heard they're going to build a huge apartment building there. I can't believe we lost our playground with all its soft grass and marigolds."`}</p>
-          </div>
-        </div>
-      </div>
-
       <div className={styles.action__block}>
-        <Button className={styles.btn}>Continue</Button>
+        <Button className={styles.btn} onClick={()=>router.push('/story')}>Continue</Button>
       </div>
     </div>
   );
