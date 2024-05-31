0b830410406a570cc4b81e655217c7ca1627ba64
surendar <surendar@willdoo.com>
2024-05-31 11:58:07 +0530

    [FIX] build fix

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -4,6 +4,7 @@ import styles from "./new.module.scss";
 import { useRouter } from "next/router";
 import Chapter from "@/components/chapter";
 import { useEffect, useRef, useState } from "react";
+import anime from "animejs";
 
 export default function Home() {
   const router = useRouter();
@@ -31,27 +32,125 @@ export default function Home() {
   // https://toby-app-dev-ui.s3.ap-south-1.amazonaws.com/images/frames/cbsc/7std/science/chpt1/blk1/poster_bg_std07_science_ch1_bl01_pnl_009/poster_bg_std07_science_ch1_bl01_pnl_009_1_5x.webp
 
   // https://toby-app-dev-ui.s3.ap-south-1.amazonaws.com/images/frames/cbsc/7std/science/chpt1/blk2/poster_bg_std07_science_ch1_bl02_pnl_001/poster_bg_std07_science_ch1_bl02_pnl_001_1_5x.webp
-
+  const easing = "easeInSine";
   const { query } = router;
+  const imageFrame = useRef<any>(null);
+  const backgroundImage = useRef<any>(null);
+  // function preloadImage (src: string) {
+  //   return new Promise((resolve, reject) => {
+  //     const img:any = new Image(src: src)
+  //     img.onload = function() {
+  //       resolve(img)
+  //     }
+  //     img.onerror = img.onabort = function() {
+  //       reject(src)
+  //     }
+  //     img.src = src
+  //   })
+  // }
 
   console.log(query.block, "QUERY");
-
+  const duration = 500;
+  const delay = 100;
   useEffect(() => {
     // search query string changed
     setStoryInfo({
       bl: query.block || 1,
       pnl: query.panel || 1,
+      no_pnl: query.no_pnl || 2
+    });
+    anime({
+      targets: backgroundImage.current,
+      duration: duration,
+      keyframes: [{ opacity: 1 }],
+      easing,
+    });
+    anime({
+      targets: imageFrame.current,
+      duration: duration,
+      keyframes: [
+        {
+          translateX: 0,
+          opacity: 1,
+        },
+      ],
+      easing,
     });
   }, [query]);
 
-  const [storyInfo, setStoryInfo] = useState({
+  const [storyInfo, setStoryInfo] = useState<any>({
     bl: query.block || 1,
     pnl: query.panel || 1,
+    no_pnl: query.no_pnl || 2
   });
   console.log(
     storyInfo.bl.toString().padStart(2, "0"),
     storyInfo.pnl.toString().padStart(3, "0")
   );
+  
+
+  const onNext = async () => {
+    anime({
+      targets: backgroundImage.current,
+      duration: duration,
+      keyframes: [{ opacity: 0.8 }],
+      easing,
+    }).finished;
+    await anime({
+      targets: imageFrame.current,
+      duration: duration,
+      keyframes: [
+        {
+          // translateX: -200,
+          opacity: 0,
+        },
+      ],
+      easing,
+    }).finished;
+    setStoryInfo({
+      bl: storyInfo.bl,
+      pnl: storyInfo.pnl + 1,
+      no_pnl: storyInfo.no_pnl,
+    });
+    // await anime({
+    //   targets: imageFrame.current,
+    //   duration: duration,
+    //   keyframes: [
+    //     {
+    //       translateX: 200,
+    //       opacity: 0,
+    //     },
+    //   ],
+    //   easing,
+    // }).finished;
+    anime({
+      targets: backgroundImage.current,
+      duration: duration,
+      delay: delay,
+      keyframes: [{ opacity: 1 }],
+      easing,
+    });
+    anime({
+      targets: imageFrame.current,
+      duration: duration,
+      delay: delay,
+      keyframes: [
+        {
+          translateX: 0,
+          opacity: 1,
+        },
+      ],
+      easing,
+    });
+  };
+
+  const onPrev = () => {
+    setStoryInfo({
+      bl: storyInfo.bl,
+      pnl: storyInfo.pnl - 1,
+      no_pnl: storyInfo.no_pnl,
+    });
+  };
 
   return (
     <>
@@ -59,6 +158,7 @@ export default function Home() {
         <div className={styles.splash_container}>
           <div className={styles.overlay_image}></div>
           <Image
+            ref={backgroundImage}
             fill
             src={`https://toby-app-dev-ui.s3.ap-south-1.amazonaws.com/images/frames/cbsc/7std/science/chpt1/blk${storyInfo.bl.toString()}/poster_bg_std07_science_ch1_bl${storyInfo.bl
               .toString()
@@ -73,17 +173,22 @@ export default function Home() {
             alt="billboard"
             priority={true}
           />
+
           <div className={styles.info_container}>
             <div className={styles.image_container}>
               <div className={styles.action_items}>
-                {storyInfo.pnl > 1 &&<Image priority src="/images/back.svg" alt="Back" onClick={() => {
-                      setStoryInfo({
-                        bl: storyInfo.bl,
-                        pnl: storyInfo.pnl - 1,
-                      });
-                    }} width={40} height={40}/>}
+                {storyInfo.pnl > 1 && (
+                  <Image
+                    priority
+                    src="/images/back.svg"
+                    alt="Back"
+                    onClick={onPrev}
+                    width={40}
+                    height={40}
+                  />
+                )}
               </div>
-              <div className={styles.image_frame}>
+              <div className={styles.image_frame} ref={imageFrame}>
                 <Image
                   fill
                   src={`https://toby-app-dev-ui.s3.ap-south-1.amazonaws.com/images/frames/cbsc/7std/science/chpt1/blk${storyInfo.bl.toString()}/poster_bg_std07_science_ch1_bl${storyInfo.bl
@@ -104,26 +209,29 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className={styles.message_container}>
-            <div className={styles.action_items}>
-                <Image priority src="/images/audio.svg" alt="Back" width={40} height={40}/>
-                <Image priority src="/images/refresh.svg" alt="Back" width={40} height={40}/>
+            <div className={styles.message__container}>
+              <div className={styles['message__action-items']}>
+                <Image
+                  priority
+                  src="/images/audio.svg"
+                  alt="Back"
+                  width={40}
+                  height={40}
+                />
+                <Image
+                  priority
+                  src="/images/refresh.svg"
+                  alt="Back"
+                  width={40}
+                  height={40}
+                />
               </div>
               <div className={styles.message_block}>
                 <div className={styles.text_block}>
                   <p>{`Can you imagine what would happen if photosynthesis didn't occur on Earth?`}</p>
                 </div>
                 <div className={styles.button_block}>
-                  <button
-                    onClick={() => {
-                      setStoryInfo({
-                        bl: storyInfo.bl,
-                        pnl: storyInfo.pnl + 1,
-                      });
-                    }}
-                  >
-                    Continue
-                  </button>
+                  <button onClick={onNext}>Continue</button>
                 </div>
               </div>
             </div>
